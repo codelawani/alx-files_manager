67f3a6e1245363601ade7197e4f93f9c2e38ae1a
Nico <lawani.gk@gmail.com>
2023-08-24 16:25:25 +0100

    implement user authorization

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,7 @@
 import redis from '../utils/redis';
 import db from '../utils/db';
 
-export class AppController {
+export default class AppController {
   static getStatus(req, res) {
     if (redis.isAlive && db.isAlive) {
       res.status(200).json({ redis: true, db: true });
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,10 @@
 import db from '../utils/db';
+import { getUserFromToken } from '../utils/helpers';
 
 const bcrypt = require('bcrypt');
+const { ObjectId } = require('mongodb');
 
-export class UsersController {
+export default class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) res.status(400).end('Missing email');
@@ -14,6 +16,20 @@ export class UsersController {
       const newUser = await usersColl.insertOne({ email, password: hashedpw });
       console.log(newUser);
       res.status(201).json({ email, id: newUser.insertedId });
+    }
+  }
+
+  static async getMe(req, res) {
+    const header = req.headers['x-token'];
+    const [userId] = await getUserFromToken(header);
+    console.log(userId);
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      const usersColl = db.client.db().collection('users');
+      const userObjId = new ObjectId(userId);
+      const user = await usersColl.findOne({ _id: userObjId });
+      res.json({ email: user.email, id: userId });
     }
   }
 }
