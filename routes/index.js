40a39fdfcbfa2f637af58c86e4bdc96b9b0e4652
Nico <lawani.gk@gmail.com>
2023-08-24 14:32:29 +0100

    handle create new user

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 import { AppController } from '../controllers/AppController';
+import { UsersController } from '../controllers/UsersController';
 
 const { Router } = require('express');
 
 const router = Router();
 
+// main
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// users
+router.post('/users', UsersController.postNew);
 
 export default router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-
+app.use(express.json());
 app.use('/', router);
 
 app.listen(PORT, () => {
