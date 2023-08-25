acd099e322a28784271ecc4da65a0babe96c14bc
Nico <lawani.gk@gmail.com>
2023-08-25 19:41:35 +0100

    implement files retrieval with pagination support

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,10 @@ router.get('/users/me', UsersController.getMe);
 // Files
 router.post('/files', FilesController.postUpload);
 
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 export default router;
